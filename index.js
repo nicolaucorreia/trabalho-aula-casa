261a77768c7e527042be02870b9174ec3c8c7937
nicolaucorreia <nicolau.correia95@gmail.com>
2021-11-29 11:40:46 -0100

    primeiro commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { constants } = require('buffer')
+const express = require('express')
+const path = require('path')
+const app = express()
+ 
+app.use(express.static('./public'))
+
+ app.get('/', function (req, response) {
+  response.sendFile(path.join(_dirname, '/public/index.html'))
+}) 
+ 
+const port = 3000;
+
+app.listen(port, function (){
+console.log("Lisenning on port: " + port)
+})
